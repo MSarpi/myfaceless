cc9cd1419ac908b8e52c7dd81efab5ea100ee239
MSarpi <miguel.sarpi@gmail.com>
2024-01-29 11:32:18 -0300

    arreglo migrate

--- database/migrations/2024_01_17_072910_create_comentarios_table.php
+++ database/migrations/2024_01_17_072910_create_comentarios_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comentarios', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('post_model_id')->constrained('post')->onDelete('cascade');
+            $table->foreignId('post_model_id')->constrained('post_models')->onDelete('cascade');
             $table->string('comentario');
             $table->timestamps();
         });
